1a9473ee7ea8ecf669658d9e78aa7b417ecd5b50
dutchmims <jameshealy901@gmail.com>
2023-05-03 21:39:34 +0000

    Set up game

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Set up the game
+grid_size = 15
+num_ships = 5
+ship_sizes = [5, 4, 3, 3, 2]
