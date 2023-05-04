5723884e4a39296eb553a80954decc1e8223a4e7
dutchmims <jameshealy901@gmail.com>
2023-05-04 19:10:56 +0000

    Corrected a few issues

--- run.py
+++ run.py
@@ -1,76 +1,82 @@
 import random
 
-# Set up the game
-grid_size = 15
-num_ships = 5
-ship_sizes = [5, 4, 3, 3, 2]
+# Function to create an empty game board
+def create_board():
+    board = []
+    for _ in range(5):
+        row = ["O"] * 5
+        board.append(row)
+    return board
 
-# Create the game grid
-grid = [['~' for _ in range(grid_size)] for _ in range(grid_size)]
+# Function to print the game board
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
 
-# Place the ships randomly on the grid
-def place_ships():
-    for size in ship_sizes:
-        while True:
-            direction = random.choice(['horizontal', 'vertical'])
-            if direction == 'horizontal':
-                row = random.randint(0, grid_size - 1)
-                col = random.randint(0, grid_size - size)
-                if all(grid[row][col+i] == '~' for i in range(size)):
-                    for i in range(size):
-                        grid[row][col+i] = '#'
-                    break
-            else:  # direction == 'vertical'
-                row = random.randint(0, grid_size - size)
-                col = random.randint(0, grid_size - 1)
-                if all(grid[row+i][col] == '~' for i in range(size)):
-                    for i in range(size):
-                        grid[row+i][col] = '#'
-                    break
-                    
-# Function to print the grid
-def print_grid():
-    print('   ' + ' '.join(str(i) for i in range(grid_size)))
-    for i in range(grid_size):
-        print(str(i) + '  ' + ' '.join(grid[i]))
+# Function to randomly place the ships on the board
+def place_ships(board):
+    ships = []
+    for _ in range(5):
+        ship_row = random.randint(0, 4)
+        ship_col = random.randint(0, 4)
+        ship = [ship_row, ship_col]
+        if ship not in ships:
+            ships.append(ship)
+            board[ship_row][ship_col] = "S"
+
+# Function to get user's guess
+def get_guess():
+    while True:
+        try:
+            guess_row = int(input("Guess Row (0-4): "))
+            guess_col = int(input("Guess Col (0-4): "))
+            if guess_row < 0 or guess_row > 4 or guess_col < 0 or guess_col > 4:
+                print("Please enter a valid guess!")
+            else:
+                return guess_row, guess_col
+        except ValueError:
+            print("Please enter valid numbers for the guess!")
+
+# Function to check if the guess hit a ship
+def check_guess(guess, ships):
+    if guess in ships:
+        ships.remove(guess)
+        return True
+    else:
+        return False
 
 # Function to play the game
 def play_game():
-    print("Welcome to Spaceship Battle")
-    print("Let's play Spaceship Battle!")
-    print("Pleae input your Spaceship Name Captain: ")
-    name = input()
-    print("Good luck, " +name+ ".")
-
-    print_grid()
-    attempts = 0
-    while True:
-        row = int(input("Enter the row coordinate: "))
-        col = int(input("Enter the column coordinate: "))
-
-        if grid[row][col] == '#':
-            print("Hit!")
-            grid[row][col] = 'X'
-            attempts += 1
-            print_grid()
-        elif grid[row][col] == '~':
-            print("Miss!")
-            grid[row][col] = 'O'
-            attempts += 1
-            print_grid()
+    print("Welcome to Space Ship Battle!")
+    print("You have 10 attempts to destroy 5 Space Ships.")
+    print("Let's Go!")
+    print()
+    
+    player_board = create_board()
+    ships = []
+    place_ships(ships)
+    attempts = 10
+    while attempts > 0:
+        print("Attempts remaining:", attempts)
+        print_board(player_board)
+        print()
+        guess = get_guess()
+        if check_guess(guess, ships):
+            print("Congratulations! You hit a ship!")
+            if len(ships) == 0:
+                print("You sank all the ships! You win!")
+                break
         else:
-            print("We are not based here, try different coordinates!")
+            print("Oops! That was a miss!")
+        attempts -= 1
+        print()
+    
+    if attempts == 0 and len(ships) > 0:
+        print("Game Over! You ran out of attempts.")
+        print("The remaining ships were located at:")
+        for ship in ships:
+            print(ship[0], ship[1])
 
-        if attempts == sum(ship_sizes):
-            print("Congratulations! You've hit and destroyed all the Space Ships!")
-            break
-
-# Start the game
-place_ships()
+# Run the game
 play_game()
-
-
-
-
-
   
